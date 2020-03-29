2e6afdda68cdfa79e49de02682422434866a1e53
Frendy <frendy@hawaii.edu>
2020-03-28 17:31:26 -1000

    Post and Get Templates

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+import { Post } from './post';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +10,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  readonly ROOT_URL = 'https://jsonplaceholder.typicode.com'
+
+  // Interface
+  //posts: Observable<Post[]>;
+
+  posts: Observable<any>;
+  newPost: Observable<any>;
+
+
+  constructor(private http: HttpClient) { }
+
+  getPosts() {
+    let params = new HttpParams().set('userId', '1');
+
+
+    // Interface
+    //this.posts = this.http.get<Post[]>(this.ROOT_URL + '/posts');
+
+    this.posts = this.http.get<Post[]>(this.ROOT_URL + '/posts', { params } );
+  }
+
+  createPost() {
+    const data: Post = {
+      id: null,
+      userId: 23,
+      title: 'My New Post',
+      body: 'Hello World!'
+    }
+
+    this.newPost = this.http.post(this.ROOT_URL + '/posts', data)
+
+  }
+
+
+
+
+
 }
